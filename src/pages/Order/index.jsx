9e56c05b403d14f2afac8ccef98c23b8c2aad19d
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2024-01-25 22:35:15 -0300

    adicionando a lógica para a tela de pedidos

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,3 +1,14 @@
+import { useSelector } from "react-redux";
+import rootReducer from "../../redux/rootReducer";
+import { Navigate } from "react-router-dom";
+
 export default function Order() {
+  const { user } = useSelector((rootReducer) => rootReducer.user);
+  alert("User " + user);
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
   return <div>tela de pedidos</div>;
 }
