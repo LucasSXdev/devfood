e481fc6248c0b41caba58f5183748a3a9b121162
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2024-02-07 22:54:24 -0300

    estruturando a api

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
 import Cart from "../components/cart/Cart";
 import Header from "../components/header";
+import api from "../api";
+import { useEffect } from "react";
 
 export default function HomeScreen() {
+  useEffect(() => {
+    getCategories = async () => {
+      const categories = api.getCategories();
+      console.log(categories);
+    };
+  }, []);
+
   return (
     <>
       <Header />
